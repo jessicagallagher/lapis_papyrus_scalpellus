12b772daf3a69200f9d66195621f6b330f662dc0
jessicagallagher <jessicalyngallagher@me.com>
2021-01-08 16:00:11 -0500

    added punctuation to results text

--- lapis_papyrus_scalpellus.js
+++ lapis_papyrus_scalpellus.js
@@ -20,25 +20,25 @@ function buttonClick(e) {
   }
   // Tests for a tie
   if (computer.currentChoice === player.currentChoice) {
-    document.getElementById("results").innerText = "The player and the computer both chose " + computer.currentChoice;
+    document.getElementById("results").innerText = "The player and the computer both chose " + computer.currentChoice + ".";
   }
   // If no tie happens then the code below will execute and determine the winner
   else if (computer.currentChoice === choices[0]) {
     if (player.currentChoice === choices[1]) {
-      document.getElementById("results").innerText = "The player wins! The computer chose " + computer.currentChoice + " and the player chose " + player.currentChoice;
+      document.getElementById("results").innerText = "The player wins! The computer chose " + computer.currentChoice + " and the player chose " + player.currentChoice + ".";
     } else {
-      document.getElementById("results").innerText = "The computer wins! The computer chose " + computer.currentChoice + " and the player chose " + player.currentChoice;
+      document.getElementById("results").innerText = "The computer wins! The computer chose " + computer.currentChoice + " and the player chose " + player.currentChoice + ".";
     }
   } else if (computer.currentChoice === choices[1]) {
     if (player.currentChoice === choices[2]) {
-      document.getElementById("results").innerText = "The player wins! The computer chose " + computer.currentChoice + " and the player chose " + player.currentChoice;
+      document.getElementById("results").innerText = "The player wins! The computer chose " + computer.currentChoice + " and the player chose " + player.currentChoice + ".";
     } else {
-      document.getElementById("results").innerText = "The computer wins! The computer chose " + computer.currentChoice + " and the player chose " + player.currentChoice;
+      document.getElementById("results").innerText = "The computer wins! The computer chose " + computer.currentChoice + " and the player chose " + player.currentChoice + ".";
     }
   } else if (computer.currentChoice === choices[2]) {
-    document.getElementById("results").innerText = "The player wins! The computer chose " + computer.currentChoice + " and the player chose " + player.currentChoice;
+    document.getElementById("results").innerText = "The player wins! The computer chose " + computer.currentChoice + " and the player chose " + player.currentChoice + ".";
   } else {
-    document.getElementById("results").innerText = "The computer wins! The computer chose " + computer.currentChoice + " and the player chose " + player.currentChoice;
+    document.getElementById("results").innerText = "The computer wins! The computer chose " + computer.currentChoice + " and the player chose " + player.currentChoice + ".";
   }
 }
 // Click event handlers
